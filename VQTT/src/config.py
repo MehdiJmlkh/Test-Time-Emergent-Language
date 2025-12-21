ee51543ea13934ab6bb39a36f0ef715244f960a7
Mehdi <mehdijmlkh1382@gmail.com>
2025-12-21 19:09:29 +0330

    Remove freeze_object_encoder config

--- VQTT/src/config.py
+++ VQTT/src/config.py
@@ -15,7 +15,6 @@ num_iterations = 5
 best_of_n = 1
 learning_rate_tt = 1e-5
 sampling_temperature_tt = 1e-5
-freeze_object_encoder = False
 number_of_candidates = 100
 commitment_weight = 0.25
 sampling_temperature = 1e-5
